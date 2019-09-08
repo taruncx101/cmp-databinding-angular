254e6e6eb51363758e0b19bdc1df89c545809e4d
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-08 12:51:59 +0530

    ngDoCheck called

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
 
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
@@ -23,4 +23,7 @@ export class ServerElementComponent implements OnInit, OnChanges {
     console.log('ngOnInit  called');
   }
 
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
 }
